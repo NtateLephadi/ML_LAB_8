7a0f731557e969dfa691932836b5d77e5cc3b1cd
Tumelo Lephadi <lphtum003@sl-dual-199.cs.uct.ac.za>
2018-10-09 11:02:24 +0200

    grid pause

--- grid.cpp
+++ grid.cpp
@@ -4,21 +4,21 @@ grid::grid(){}
 
 grid::~grid(){};
 
-int grid::next_value(state s){
-    state next;
-    for (state s1: this->world){
-      if (s1.get_state_number()==s.get_state_number()) {
-        /* code */
-        next = s1;
-        break;
-      }
-    }
-    return next.get_value();
-}
-
-void grid::fill_grid(state s){
-  int reward = s.get_reward(s.get_state_number());
-  float discount = 0.8;
-  int value = next_value(s);
-  value = reward + discount*value;
-}
+// int grid::next_value(state s){
+//     state next;
+//     for (state s1: this->world){
+//       if (s1.get_state_number()==s.get_state_number()) {
+//         /* code */
+//         next = s1;
+//         break;
+//       }
+//     }
+//     return next.get_value();
+// }
+//
+// void grid::fill_grid(state s){
+//   int reward = s.get_reward(s.get_state_number());
+//   float discount = 0.8;
+//   int value = next_value(s);
+//   value = reward + discount*value;
+// }
